f7491308b7180a8442b7abaa54d91d162422cbe2
Alex P <alex.pantec@gmail.com>
2017-01-12 22:47:48 +0200

    main, usage, validation

--- src/main/kotlin/minic/Main.kt
+++ src/main/kotlin/minic/Main.kt
@@ -1,22 +1,76 @@
 package minic
 
-import minic.frontend.antlr.MiniCLexer
-import minic.frontend.antlr.MiniCParser
-import org.antlr.v4.runtime.ANTLRInputStream
-import org.antlr.v4.runtime.CommonTokenStream
+import org.apache.commons.io.FilenameUtils
+import java.io.FileInputStream
+
+val compilerName = "minic"
+
+fun showUsage() {
+    println(
+"""Usage:
+$compilerName input_file [output_file]
+    Compiles specified source code into file with JVM bytecode.
+    input_file
+        Path (or name) of file with Mini-C source code.
+    output_file
+        Optional. Path (or name) of output file with JVM bytecode.
+        If not specified, input_file without extension will be used.
+        .class extension is appended if not present (otherwise java will
+         not run it), such as MyProgram.class.
+    Use java to run it (java MyProgram).
+$compilerName --help (or -help, help, -h, --h)
+    Prints this information.
+If launched without arguments, reads input from stdin
+until EOF (Ctrl+D, or Ctrl+Z for Windows), compiles and runs the program.
+Examples:
+    $compilerName MyProgram.mc
+    $compilerName MyProgram.mc MyProgram""")
+}
 
 fun main(args: Array<String>) {
     println("Mini-C compiler")
-    println("Enter code and press Ctrl+D (Ctrl+Z for Windows)")
-    println()
 
-    val input = ANTLRInputStream(System.`in`)
+    var inputStream = System.`in`
 
-    val lexer = MiniCLexer(input)
+    if (args.count() > 0) {
+        if (args.count() > 2) {
+            println("Incorrect arguments")
+            showUsage()
+            return
+        }
 
-    val tokens = CommonTokenStream(lexer)
+        if (args.any { listOf("help", "--help", "-help", "-h", "--h").contains(it) }) {
+            showUsage()
+            return
+        }
 
-    val parser = MiniCParser(tokens)
+        val inputFilePath = args[0]
 
-    parser.program()
+        var outputFilePath = if (args.count() > 1) args[1] else FilenameUtils.removeExtension(inputFilePath)
+        if (!outputFilePath.endsWith(".class"))
+            outputFilePath += ".class"
+
+        println("Input file: $inputFilePath")
+        println("Output file: $outputFilePath")
+
+        inputStream = FileInputStream(inputFilePath)
+    } else {
+        println("Enter code and press Ctrl+D (Ctrl+Z for Windows)")
+        println()
+    }
+
+    val compiler = Compiler()
+
+    val parsingResult = compiler.parse(inputStream)
+
+    val errors = compiler.validate(parsingResult)
+    if (errors.any()) {
+        println("${errors.count()} error${if (errors.count() > 1) "s" else ""}.")
+        errors.forEach {
+            println("Line ${it.position.line}:${it.position.column}: ${it.message}")
+        }
+        return
+    }
+
+
 }
