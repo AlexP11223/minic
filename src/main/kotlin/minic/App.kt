8d66ca43d7afec61cfc396ec40ec2c54111f9635
Alex P <alex.pantec@gmail.com>
2017-04-23 14:47:16 +0300

    main refactoring, tests, option parser

---
+++ src/main/kotlin/minic/App.kt
@@ -0,0 +1,161 @@
+package minic
+
+import joptsimple.OptionParser
+import joptsimple.OptionSpec
+import org.apache.commons.io.FilenameUtils
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.io.PrintStream
+
+internal class App(val out: PrintStream = System.out,
+                   val err: PrintStream = System.err,
+                   val input: InputStream = System.`in`) {
+
+    private val compilerName = "minic"
+
+    private val optionParser = OptionParser()
+    private val helpOption: OptionSpec<Void>
+    private val astOption: OptionSpec<String>
+    private val tokensOption: OptionSpec<Void>
+
+    init {
+        helpOption = optionParser.acceptsAll(arrayListOf("h", "help", "?"), "Show help").forHelp()
+        astOption = optionParser.accepts("ast", "Draw AST and save as PNG image (default ast.png)")
+                .withOptionalArg().defaultsTo("ast.png").describedAs("png_output_file")
+        tokensOption = optionParser.accepts("tokens", "Output lexer tokens")
+    }
+
+    fun run(args: Array<String>) {
+        val options = try {
+            optionParser.parse(*args)
+        } catch (ex: Exception) {
+            err.println(ex.message)
+            showUsage()
+            return
+        }
+
+        val nonOptionArgs = options.nonOptionArguments().map { it.toString() }
+
+        if (options.has(helpOption) || nonOptionArgs.contains("help")) {
+            showUsage()
+            return
+        }
+
+        if (nonOptionArgs.count() > 2) {
+            err.println("Incorrect arguments")
+            showUsage()
+            return
+        }
+
+        val executionMode = nonOptionArgs.isEmpty()
+
+        var inputStream = input
+        var outputFilePath: String? = null
+
+        if (nonOptionArgs.any()) {
+            val inputFilePath = nonOptionArgs[0]
+
+            outputFilePath = if (nonOptionArgs.count() > 1) nonOptionArgs[1] else FilenameUtils.removeExtension(inputFilePath)
+            if (!outputFilePath!!.endsWith(".class"))
+                outputFilePath += ".class"
+
+            out.println("Input file: $inputFilePath")
+            out.println("Output file: $outputFilePath")
+
+            if (!File(inputFilePath).isFile) {
+                err.println("File not found.")
+                return
+            }
+
+            inputStream = FileInputStream(inputFilePath)
+        }
+
+        if (executionMode) {
+            out.println("Enter code and press Ctrl+D (Ctrl+Z for Windows)")
+            out.println()
+        }
+
+        val compiler = Compiler(inputStream)
+
+        val errors = compiler.validate()
+        if (errors.any()) {
+            out.println("${errors.count()} error${if (errors.count() > 1) "s" else ""}.")
+            errors.forEach {
+                out.println("Line ${it.position.line}:${it.position.column}: ${it.message}")
+            }
+            return
+        }
+
+        try {
+            if (executionMode) {
+                compiler.execute()
+            } else {
+                compiler.compile(outputFilePath!!)
+            }
+        } catch (ex: Exception) {
+            err.println("Code generation error")
+            err.println(ex.message)
+            ex.printStackTrace()
+            return
+        }
+
+        if (options.has(tokensOption)) {
+            out.println("Tokens:")
+            compiler.tokens.forEach {
+                out.println("${it.name}: ${it.text}")
+            }
+        }
+
+        if (options.has(astOption)) {
+            val astOutputFilePath = astOption.value(options)
+            compiler.drawAst(astOutputFilePath)
+            out.println("Saved AST to $astOutputFilePath")
+        }
+    }
+
+
+    private fun showUsage() {
+        err.println(
+                """Usage:
+$compilerName [input_file [output_file]] [options]
+    Compiles specified source code into file with JVM bytecode.
+    input_file
+        Path (or name) of file with Mini-C source code.
+    output_file
+        Optional. Path (or name) of output file with JVM bytecode.
+        If not specified, input_file without extension will be used.
+        .class extension is appended if not present (otherwise java will
+         not run it), such as MyProgram.class.
+    Use java to run it (java MyProgram).
+If launched without these arguments, reads input from stdin
+until EOF (Ctrl+D, or Ctrl+Z for Windows), compiles and runs the program.""")
+
+        err.println("Options:")
+        val indent = "    "
+        optionParser.formatHelpWith({ optionsDescrMap ->
+            optionsDescrMap.values.distinct()
+                    .filter { !it.representsNonOptions() }
+                    .map {
+                         indent + it.options()
+                                 .map {
+                                     val prefix = if (it.length > 1) "--" else "-"
+                                     prefix + it
+                                 }.joinToString(", ") +
+                                 (if (it.acceptsArguments())
+                                     (" " + if (it.requiresArgument()) "<${it.argumentDescription()}>" else "[${it.argumentDescription()}]")
+                                 else "") +
+                                 System.lineSeparator() + indent + indent + it.description()
+                    }
+                    .joinToString(System.lineSeparator()) + System.lineSeparator()
+        })
+        optionParser.printHelpOn(err)
+
+        err.println("""Examples:
+    $compilerName MyProgram.mc
+    $compilerName MyProgram.mc MyProgram
+    $compilerName MyProgram.mc --tokens""")
+    }
+
+
+}
---
+++ src/test/kotlin/minic/AppTest.kt
@@ -0,0 +1,172 @@
+package minic
+
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.Assert.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.PrintStream
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+import kotlin.test.assertTrue
+
+class AppTest {
+    var output: String = ""
+    var errorOutput: String = ""
+
+    @Rule
+    @JvmField
+    val tmpFolder = TemporaryFolder()
+
+    private fun run(args: Array<String>, input: String = "") {
+        val inBaos = ByteArrayOutputStream()
+        val errBaos = ByteArrayOutputStream()
+        val inputStream = if (input.isEmpty()) System.`in` else IOUtils.toInputStream(input, StandardCharsets.UTF_8)
+
+        App(PrintStream(inBaos), PrintStream(errBaos), inputStream).run(args)
+
+        output = String(inBaos.toByteArray(), StandardCharsets.UTF_8)
+        errorOutput = String(errBaos.toByteArray(), StandardCharsets.UTF_8)
+    }
+
+    private fun assertShowsHelp() {
+        assertThat(errorOutput, containsString("Usage:"))
+        assertThat(errorOutput, containsString("Options:"))
+        assertThat(errorOutput, containsString("--ast"))
+        assertThat(errorOutput, containsString("--tokens"))
+        assertThat(errorOutput, containsString("Examples:"))
+    }
+
+    @Test
+    fun showsHelpWhenIncorrectOption() {
+        run(arrayOf("--unknownflag"))
+
+        assertThat(errorOutput, containsString("unknownflag"))
+        assertThat(errorOutput, containsString("recognized"))
+        assertShowsHelp()
+
+        run(arrayOf("-q"))
+
+        assertThat(errorOutput, containsString("q"))
+        assertThat(errorOutput, containsString("recognized"))
+        assertShowsHelp()
+    }
+
+    @Test
+    fun showsHelpWhenTooMuchArgs() {
+        run(arrayOf("arg1", "arg2", "arg3"))
+
+        assertShowsHelp()
+    }
+
+    @Test
+    fun showsErrorWhenInputFileNotFound() {
+        run(arrayOf("notexisting.mc"))
+
+        assertThat(errorOutput, containsString("not found"))
+    }
+
+    @Test
+    fun compilesToDefaultOutputPath() {
+        val inputFilePath = tmpFolder.root.absolutePath + "/Program.mc"
+        val outputFilePath = tmpFolder.root.absolutePath + "/Program.class"
+
+        FileUtils.writeStringToFile(File(inputFilePath), "print(\"Hello\");", Charset.defaultCharset())
+
+        run(arrayOf(inputFilePath))
+
+        assertTrue(errorOutput.isEmpty(), errorOutput)
+        assertTrue(File(outputFilePath).exists(), "$outputFilePath doesn't exist")
+        assertTrue(File(outputFilePath).length() > 0, "$outputFilePath is empty")
+    }
+
+    @Test
+    fun compilesToCustomOutputPath() {
+        val inputFilePath = tmpFolder.root.absolutePath + "/Program.mc"
+        val outputFilePath = tmpFolder.root.absolutePath + "/MyProgram.class"
+
+        FileUtils.writeStringToFile(File(inputFilePath), "print(\"Hello\");", Charset.defaultCharset())
+
+        run(arrayOf(inputFilePath, outputFilePath))
+
+        assertTrue(errorOutput.isEmpty(), errorOutput)
+        assertTrue(File(outputFilePath).exists(), "$outputFilePath doesn't exist")
+        assertTrue(File(outputFilePath).length() > 0, "$outputFilePath is empty")
+    }
+
+    @Test
+    fun executesFromInput() {
+        run(arrayOf(), "int x = 42;")
+
+        assertTrue(errorOutput.isEmpty(), errorOutput)
+
+        assertThat(output, containsString("Ctrl+D"))
+    }
+
+    @Test
+    fun compilesAndShowsTokensAst() {
+        val inputFilePath = tmpFolder.root.absolutePath + "/Program.mc"
+        val astOutputFilePath = tmpFolder.root.absolutePath + "/ast.png"
+
+        FileUtils.writeStringToFile(File(inputFilePath), "int x = 42;\n int y = x*2;", Charset.defaultCharset())
+
+        val argsCombinations = listOf(
+                arrayOf(inputFilePath, "--tokens", "--ast", astOutputFilePath),
+                arrayOf(inputFilePath, "--ast", astOutputFilePath, "--tokens")
+        )
+
+        argsCombinations.forEach { args ->
+            run(args)
+
+            assertTrue(errorOutput.isEmpty(), errorOutput)
+
+            assertThat(output, containsString("Tokens:"))
+            assertThat(output, containsString("INT_TYPE"))
+            assertThat(output, containsString("ASSIGN"))
+            assertThat(output, containsString("MUL"))
+
+            assertTrue(File(astOutputFilePath).exists(), "$astOutputFilePath doesn't exist")
+            assertTrue(File(astOutputFilePath).length() > 0, "$astOutputFilePath is empty")
+
+            File(astOutputFilePath).delete()
+        }
+    }
+
+    @Test
+    fun executesAndShowsTokensAst() {
+        val astOutputFilePath = tmpFolder.root.absolutePath + "/ast.png"
+
+        run(arrayOf("--tokens", "--ast", astOutputFilePath), "int x = 42;\n int y = x*2;")
+
+        assertTrue(errorOutput.isEmpty(), errorOutput)
+
+        assertThat(output, containsString("Tokens:"))
+        assertThat(output, containsString("INT_TYPE"))
+        assertThat(output, containsString("ASSIGN"))
+        assertThat(output, containsString("MUL"))
+
+        assertTrue(File(astOutputFilePath).exists(), "$astOutputFilePath doesn't exist")
+        assertTrue(File(astOutputFilePath).length() > 0, "$astOutputFilePath is empty")
+    }
+
+    @Test
+    fun showsCompilationErrors() {
+        val inputFilePath = tmpFolder.root.absolutePath + "/Program.mc"
+        val outputFilePath = tmpFolder.root.absolutePath + "/Program.class"
+
+        FileUtils.writeStringToFile(File(inputFilePath), "int a = b;", Charset.defaultCharset())
+
+        run(arrayOf(inputFilePath))
+
+        assertTrue(errorOutput.isEmpty(), errorOutput)
+
+        assertThat(output, containsString("error"))
+        assertThat(output, containsString("declared"))
+
+        assertTrue(!File(outputFilePath).exists(), "$outputFilePath exists")
+    }
+}
